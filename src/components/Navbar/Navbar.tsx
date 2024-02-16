b68d1be14a9ec47f5bb710639dff7b5d29babd6f
Marcin Barc <marb637@gmail.com>
2024-02-16 19:22:23 +0100

    tailwind with config for vite added to project

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-
 export const Navbar = () => {
     return (
-        <div>
-            <h1>React</h1>
+        <div className='text-slate-50'>
+            <h1 className=' w-full text-3xl font-bold text-[#00df9a]'>React</h1>
+            <ul className='flex'>
+                <li className='p-4'>Company</li>
+                <li className='p-4'>Home</li>
+                <li className='p-4'>Resources</li>
+                <li className='p-4'>About</li>
+                <li className='p-4'>Contact</li>
+            </ul>
         </div>
     );
 };
