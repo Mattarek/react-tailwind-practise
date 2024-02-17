1cd1d36b9054dffc8988af9c7836963ea08aadd7
Marcin Barc <marb637@gmail.com>
2024-02-17 13:09:01 +0100

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { Hero } from './components/Hero/Hero';
 import { Navbar } from './components/Navbar/Navbar';
 import { Footer } from './components/Footer/Footer';
 import { Cards } from './components/Cards/Cards';
+import { Analytics } from './components/Analytics/Analytics';
+import { Newsletter } from './components/Newsletter/Newsletter';
 
 function App() {
     return (
         <>
             <Navbar />
             <Hero />
+            <Analytics />
             <Cards />
+            <Newsletter />
             <Footer />
         </>
     );
